dbbdc6f5f8647fe19ecb90db93b775f98176314f
Kauan Aprigio Estevão <k260205@dac.unicamp.br>
2025-03-13 15:28:12 -0300

    melhorei algumas coisas

--- Ambiente.java
+++ Ambiente.java
@@ -1,20 +1,21 @@
 public class Ambiente {
 
-    int largura;
-    int altura;
-    String terreno[][] = new String[largura][altura];
+    int largura = 3;
+    int altura = 4;
+    String terreno[][] ; 
 
     
-    public void Construtor_Ambiente(int larguraX, int alturaY, String ambiente[][]){
+    public void Construtor_Ambiente(int larguraX, int alturaY){
+        this.largura = larguraX;
+        this.altura = alturaY;
+        this.terreno = new String[larguraX][alturaY];
+
         for (int i = 0; i < larguraX; i++){
             for (int j = 0; j < alturaY; j++){
-                terreno[larguraX][alturaY] = "_";
+                terreno[i][j] = "_";
             }
         }
         
-        this.largura = larguraX;
-        this.altura = alturaY;
-        this.terreno = ambiente;
     }
 
     public boolean dentroDosLimites(int x, int y){
@@ -23,6 +24,15 @@ public class Ambiente {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public void imprimirAmbiente() {
+        for (int i = 0; i < largura; i++) {
+            for (int j = 0; j < altura; j++) {
+                System.out.print(terreno[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
