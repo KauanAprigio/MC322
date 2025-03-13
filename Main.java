022c96eb8000ffd8eae5e40a0d8931173d562e13
Kauan Aprigio Estevão <k260205@dac.unicamp.br>
2025-03-13 09:55:45 -0300

    Primeiro push

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nois que sabe o programa karai");
+        System.out.println("Nois que sabe o programa karai ");
     }
     
 }
