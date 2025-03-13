3c32dd135f27a60332a055a69128f6e049cdd029
dieg0o37 <d288809@dac.unicamp.br>
2025-03-13 15:13:48 -0300

    Adiciona classe robo e um caso teste

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Nois que sabe o programa karai ");
+    public static void main(String[] args){
+        Robo robo1 = new Robo("diego", 0, 0);
+        robo1.mover(2, 2);
+        robo1.exibirPosicao();
     }
-// os mlk é liso
 }
+
---
+++ Robo.java
@@ -0,0 +1,21 @@
+
+public class Robo {
+    private String nome;
+    private int posicaoX;
+    private int posicaoY;
+
+    public Robo(String nome, int posicaoX, int posicaoY) {
+        this.nome = nome;
+        this.posicaoX = posicaoX;
+        this.posicaoY = posicaoY;
+    }
+
+    public void mover(int deltaX, int deltaY) {
+        posicaoX += deltaX;
+        posicaoY += deltaY;
+    }
+
+    public void exibirPosicao() {
+        System.out.printf("Posição atual do robõ %s: (%d, %d)\n", nome, posicaoX, posicaoY);
+    }
+}
