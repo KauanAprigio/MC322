c1109eea50c3eb854f4bd8076e3e1ef7149eb81b
DIEGO <d288809@dac.unicamp.br>
2025-03-14 12:25:16 -0300

    alterei e consertei o construtor, e coloquei uma fuunção getNome

--- Ambiente.java
+++ Ambiente.java
@@ -1,28 +1,20 @@
 public class Ambiente {
 
-    int largura;
-    int altura;
-    String terreno[][] = new String[largura][altura];
-
+    private int largura;
+    private int altura;
+    private String nome;
     
-    public void Construtor_Ambiente(int larguraX, int alturaY, String ambiente[][]){
-        for (int i = 0; i < larguraX; i++){
-            for (int j = 0; j < alturaY; j++){
-                terreno[larguraX][alturaY] = "_";
-            }
-        }
-        
+    public Ambiente(int larguraX, int alturaY, String nome) {
         this.largura = larguraX;
         this.altura = alturaY;
-        this.terreno = ambiente;
+        this.nome = nome;
     }
 
-    public boolean dentroDosLimites(int x, int y){
-
-        if ((0 <= x && x < largura) && (0 <= y && y < altura)){
-            return true;
-        } else {
-            return false;
-        }
+    public boolean dentroDosLimites(int x, int y) {
+        if ((0 <= x && x < largura) && (0 <= y && y < altura)) return true;
+        return false;
+    }  
+    public String getNome() {
+        return nome;
     }
 }
