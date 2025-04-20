f2914faf36fc1578c6cd8acc90770383075ef528
DIEGO <d288809@dac.unicamp.br>
2025-04-20 14:21:46 -0300

    Add Sensor class to define sensor properties and behavior

---
+++ LAB03/Code/Sensor.java
@@ -0,0 +1,17 @@
+package LAB03.Code;
+
+/*
+ * Classe obrigatória Sensor
+ * 
+ * Atributos:
+ * - raio (double)
+ * 
+ * Métodos:
+ * - Monitorar()
+ * 
+ * Relação com a classe Robo: 
+ * - Um robô tem 1 ou vários sensores (Composição)
+ */
+public class Sensor {
+    
+}
