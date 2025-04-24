6872215b3af328b6301aae1856afae3416599b9e
DIEGO <d288809@dac.unicamp.br>
2025-04-24 20:15:47 -0300

    Refactor posicao_segura method in SensorPosicaoSegura class: add boundary checks for environment and radius, and improve safety position validation logic.

--- LAB03/Code/SensorPosicaoSegura.java
+++ LAB03/Code/SensorPosicaoSegura.java
@@ -18,11 +18,21 @@ public class SensorPosicaoSegura extends Sensor {
     }
 
     //Metodos
-    public boolean posicao_segura(int pos_x, int pos_y){
+    public boolean posicao_segura(int pos_x, int pos_y, int x_atual, int y_atual) {
+        // verifica se a posição está dentro do ambiente e dentro do raio
+        if (pos_x < 0 || pos_x > ambiente.getLargura() || pos_y < 0 || pos_y > ambiente.getAltura()){
+            System.out.println("Posição fora do ambiente");
+            return false; // se não está dentro do ambiente retorna false
+        }
+        if (Math.sqrt(Math.pow((pos_x - x_atual), 2) + Math.pow((pos_y - y_atual), 2)) > getRaio()){
+            System.out.println("Posição fora do raio");
+            return false; // se não está dentro do raio retorna false
+        }
         // aqui vejo todos os obstáculos e caso tenha algum que esteja na posição, a posicao já não é segura
         for(Obstaculo o : ambiente.getObstaculos()){ 
             if (pos_x >= o.getPosicaoX1() && pos_x <= o.getPosicaoX2()
             && pos_y >= o.getPosicaoY1() && pos_y <= o.getPosicaoY2()){
+                System.out.println("Posição não segura");
                 return false; // se não é segura retorna false
             } 
         }
