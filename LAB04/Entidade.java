82b9c2db784e62ccaaf0b460ae68c2c4a7d2a2c8
Kauan Aprigio Estevão <k260205@dac.unicamp.br>
2025-05-12 21:35:16 -0300

    Apenas criei os arquivos

---
+++ LAB04/Entidade.java
@@ -0,0 +1,7 @@
+package LAB04;
+
+
+//so para lembrar que aqui primeiro fazer a entidade depois fazer o enum TipoEntidade
+public interface Entidade {
+    
+}
---
+++ LAB04/Main.java
@@ -0,0 +1,5 @@
+package LAB04;
+
+public class Main {
+    
+}
