b194e9a7f8c8e6d6f0117f5c938396f6e8e630c2
Kauan Aprigio Estevão <k260205@dac.unicamp.br>
2025-05-20 15:27:25 -0300

    Modifiquei o metodo para adicionar as entidades nas representações 2d e 3d

--- LAB04/Ambiente.java
+++ LAB04/Ambiente.java
@@ -15,14 +15,16 @@ public class Ambiente {
     int altura; // eixo Z
     ArrayList<Entidade> entidades;
     TipoEntidade[][][] mapa;
+    char[][] planoXY; // aqui irei usar esse plano para representar o ambiente 2d, no caso o plano xy com z = 0
 
     
     //Construtor
-    Ambiente(int largura, int profundidade, int altura, TipoEntidade[][][] mapa){
+    Ambiente(int largura, int profundidade, int altura, TipoEntidade[][][] mapa, char[][] planoXY){
         this.largura = largura;
         this.profundidade = profundidade;
         this.altura = altura;
         this.mapa = mapa;
+        this.planoXY = planoXY;
         entidades = new ArrayList<Entidade>();
     }
 
@@ -31,6 +33,7 @@ public class Ambiente {
     public void inicializarMapa(){
         for (int x = 0; x < largura; x++){
             for (int y = 0; y < profundidade; y++){
+                planoXY[x][y] = 'v'; // aqui irei fazer a representação do plano xy no z = 0
                 for (int z = 0; z < altura; z++){
                     mapa[x][y][z] = TipoEntidade.VAZIO;
                 }
@@ -46,9 +49,15 @@ public class Ambiente {
                 return; // aqui acaba o método caso seja fora dos limites
             }
         // aqui significa que está nos limites, logo ira adicionar
+
+        //vou deixar separado do resto para melhor visualização 
+        mapa[e.getX()][e.getY()][e.getZ()] = TipoEntidade.ROBO; // adiciona na representação do ambiente
+        planoXY[e.getX()][e.getY()] = r.getRepresentacao(); // adiciona no planoXY, ou seja, representaçaõ 2d do ambiente
         entidades.add(e);
+        
         System.out.println("A entidade ROBO, de ID" + r.getId() + ", foi adicionado ao ambiente.");
         System.out.println("Posição: (" + r.getX() + ", " + r.getY() + ", " + 0 + ")\n");
+        
         } else if (e.getTipo() == TipoEntidade.OBSTACULO){
             Obstaculo o = (Obstaculo) e; // aqui usarei o casting para ter acesso aos metodos getPosx2 e y2
             if (!dentroDosLimites(o.getX(), o.getY(), o.getZ()) || !dentroDosLimites(o.getPosicaoX2(), o.getPosicaoY2(), o.getZ())) {
@@ -56,6 +65,23 @@ public class Ambiente {
                 //PODEMOS USAR RECURSAO PARA CHAMAR A FUNÇÃO ATÉ DAR UMA ENTIDADE QUE ESTEJA NOS LIMITES
                 return; // aqui acaba o método caso seja fora dos limites
             }
+            
+            //Aqui irei colocar a entidade OBJETO na representação 2d e 3d do ambiente
+            for (int x = o.getX(); x < o.getPosicaoX2(); x++) {
+                for (int y = o.getY(); y < o.getPosicaoY2(); y++) {
+                    planoXY[x][y] = o.getRepresentacao(); // Representação no plano XY (z=0)
+                    
+                    if (o.getZ() > 0) { // Preencherá as camadas no eixo z se o objeto tiver altura
+                        for (int z = 0; z < o.getZ(); z++) {
+                            mapa[x][y][z] = TipoEntidade.OBSTACULO;
+                        }
+                    } else { //Se nao tiver altura preenche somente o z = 0 para deixar o código mais eficiente
+                        mapa[x][y][0] = TipoEntidade.OBSTACULO;
+                    }
+                }
+            }
+
+
             entidades.add(e);
             System.out.println("A entidade OBSTACULO, de tipo" + o.getTipoObstaculo() + ", foi adicionado ao ambiente.");
             System.out.println("Posição inferior esquerda: (" + o.getX() + ", " + o.getY() + ", " + 0 + ")");
@@ -63,8 +89,9 @@ public class Ambiente {
         }
     }
 
-    public void removerEntidade(Entidade e){ //pode desenvolver uma exception se quiser
-        
+    public void removerEntidade(Entidade e){ //pode desenvolver uma exception se quiser, no caso se eu for remover algo que nao existe
+        entidades.remove(e);
+        System.out.println("A Entidade " + e.getTipo() + " foi removida com sucesso.");
     }
 
     public boolean dentroDosLimites(int x, int y, int altitude) { // ve se esta dentro dos limites de x,y e altitude, caso contrário retorna false
@@ -72,24 +99,29 @@ public class Ambiente {
         return false;
     }
 
-    void estaOcupado(int x, int y, int z){
+    public void estaOcupado(int x, int y, int z){
         // a principio acho que seria uma função para ver se tem algo na posicao, da pra fazer como se fosse a ideia do sensor de posicao segura 
     }
 
-    void moverEntidade(Entidade e, int novoX, int novoY, int novoZ ){
+    public void moverEntidade(Entidade e, int novoX, int novoY, int novoZ ){
         // acho que seria algo para mover obstaculos...
     }
 
-    void executarSensores(){
+    public void executarSensores(){
         
     }
 
-    void verificarColisoes(){
+    public void verificarColisoes(){
         //pode ser o sensor de proximidade? ou tem q implementar isso?
     }
 
-    void visualizarAmbiente(){
-        // imprime o ambiente em 2d, somente eixo (X,Y)
+    public void visualizarAmbiente(){
+        for (int x = 0; x < largura; x++){
+            for (int y = 0; y < profundidade; y++){
+                System.out.println(planoXY[x][y] + " ");// aqui irei printar os caracteres dando um espaço entre eles
+            }
+        }
+        System.err.println(); // uma quebra de linha para deixar dividido a matriz do resto na main
     }
 
 
