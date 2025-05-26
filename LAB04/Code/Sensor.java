9c8dd458e6d9ac94beb5f15b187df1616ac91720
DIEGO <d288809@dac.unicamp.br>
2025-05-26 14:45:58 -0300

    transformei o sensor em uma classe abstrata ja que não estava sendo utilizado

--- LAB04/Code/Sensor.java
+++ LAB04/Code/Sensor.java
@@ -1,19 +1,6 @@
 package LAB04.Code;
-import LAB04.Code.Entidade.TipoEntidade;
 
-/*
- * SuperClasse obrigatória Sensor
- * 
- * Atributos:
- * - double raio 
- * 
- * Métodos:
- *  Encontra o ponto mais próximo do sensor que pertence a um obstáculo
- *  Se o ponto mais próximo estiver dentro do raio do sensor detecta ele
- * - Monitorar(int X, int Y, int altura, Ambiente ambiente) // detecta se há algum obstáculo na área de monitoramento
- * 
- */
-public class Sensor {
+public abstract class Sensor {
     private double raio;
 
     // Construtor
@@ -21,32 +8,8 @@ public class Sensor {
         this.raio = raio;
     }
     // Métodos
-    public void monitorar(int x, int y, int altura, Ambiente ambiente) {
-        int N_obstaculos = 0;
-        // Lógica para monitorar o ambiente
-        // Conta os obstáculos na área de monitoramento
-        
-        //terei de mudar basntate por conta da mudança do ambiente, mas depois vejo isso 
-        for (Entidade e: ambiente.getEntidades()) {
-            if (e.getTipo() == TipoEntidade.OBSTACULO){
-                Obstaculo o = (Obstaculo) e; // casting para que eu possa usar getPosicaox2 e y2 para calcular as distancias
-                int Xmaisproximo = Math.max(o.getX(), Math.min(x, o.getPosicaoX2()));
-                int Ymaisproximo = Math.max(o.getY(), Math.min(y, o.getPosicaoY2()));
-                int Zmaisproximo = Math.max(0, Math.min(altura, o.getZ()));
-                double distancia = Math.sqrt(Math.pow(Xmaisproximo - x, 2) + Math.pow(Ymaisproximo - y, 2) + Math.pow(Zmaisproximo - altura, 2));
-                if (distancia <= raio) {
-                    N_obstaculos++;
-                }
-            }
-        }
-        if (N_obstaculos > 0) {
-            System.out.println("Sensor detectou " + N_obstaculos + " obstáculos próximos.\n");
-        } else {
-            System.out.println("Sensor não detectou obstáculos próximos!\n");
-        }
-    }
+    public abstract void monitorar(int x, int y, int altura, Ambiente ambiente);
 
     // Getters e Setters
     public double getRaio() { return raio; }
-    public void setRaio(double raio) { this.raio = raio; }
 }
