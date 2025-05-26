cf95de65a5af56792f960517599c1c01b168088b
Kauan Aprigio Estevão <k260205@dac.unicamp.br>
2025-05-26 01:20:11 -0300

    Fiz mais mudanças no comunicador central

--- LAB04/Code/ComunicadorCentral.java
+++ LAB04/Code/ComunicadorCentral.java
@@ -3,6 +3,7 @@ package LAB04.Code;
 import java.util.ArrayList;
 
 import LAB04.Code.Exceptions.RoboDesligadoException;
+import LAB04.Code.Obstaculo.TipoObstaculo;
 
 public class ComunicadorCentral extends CentralComunicacao implements Entidade, Comunicavel{
     private TipoEntidade tipo;
@@ -29,6 +30,46 @@ public class ComunicadorCentral extends CentralComunicacao implements Entidade, 
         //lixos = new ArrayList<Obstaculo>();
     }
     
+    public void listarFogos(){
+        for(Entidade e : getAmbiente().getEntidades()){
+            if (e.getTipo() == TipoEntidade.OBSTACULO){
+                Obstaculo o = (Obstaculo) e;
+                if (o.getTipoObstaculo().isFogo()){
+                    fogos.add(o);
+                }
+            }
+        }
+    }
+
+    //basicamente ele vai ver em relacao ao robo/entidade onde ele está e com isso onde está o fogo mais próximo
+    public void avisoFogoProximo(Entidade e){
+        if (e.getTipo() == TipoEntidade.ROBO){ // vê se é um robo, talvez tenha que ver se é RoboBombeiro
+            Obstaculo fogoProximo = null;
+            double menorDistancia = Double.MAX_VALUE; // Inicializa com o maior valor possível para double
+            for (Obstaculo o : fogos) {
+                // Calcula a distância euclidiana entre o robô e o obstáculo
+                // Assumindo que getX() e getY() do obstáculo retornam um ponto representativo (ex: centro ou canto)
+                int Xmaisproximo = Math.max(o.getX(), Math.min(e.getX(), o.getPosicaoX2()));
+                int Ymaisproximo = Math.max(o.getY(), Math.min(e.getY(), o.getPosicaoY2()));
+                int Zmaisproximo = Math.max(0, Math.min(e.getZ(), o.getAlturinha()));
+                double distancia = Math.sqrt(Math.pow(Xmaisproximo - e.getX(), 2) + Math.pow(Ymaisproximo - e.getY(), 2) + Math.pow(Zmaisproximo - e.getZ(), 2));
+                    
+                if (distancia < menorDistancia) {
+                    menorDistancia = distancia;
+                    fogoProximo = o;
+                }
+            }
+            RoboBombeiro robozin = (RoboBombeiro) e;
+            String mensagemFogo = "O fogo mais próximo do robo de ID " + robozin.getId() + " é o obstaculo "
+                                   + fogoProximo.getTipoObstaculo() + " com coordenadas em (" + fogoProximo.getX() 
+                                   + "," + fogoProximo.getY() + ").\n" ;
+            Comunicavel robozao = (Comunicavel) robozin;
+            enviarMensagem(robozao, mensagemFogo);
+        }
+
+        
+    }
+
     // Metodos sobrescritos da Comunicavel
     @Override
     public void enviarMensagem(Comunicavel destinatario, String mensagem){
