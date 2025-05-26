99286ef3c080c22a56ed2c449e4850e4c120d88f
DIEGO <d288809@dac.unicamp.br>
2025-05-26 14:47:07 -0300

    adiciona um verificador para ver se a entidade é comunicavel. Por padrão é falso

--- LAB04/Code/Entidade.java
+++ LAB04/Code/Entidade.java
@@ -7,15 +7,22 @@ public interface Entidade {
     public int getX();
     public int getY();
     public int getZ();
+
     public TipoEntidade getTipo();
+
     public String getDescricao();
+
     public char getRepresentacao();
+    
     public void mover(int deltaX, int deltaY, int deltaZ) throws RoboDesligadoException;
     public int getLarguraX ();
     public int getLarguraY ();
     public int getAltura();
     public String getId();
     public Ambiente getAmbiente();
+    public default boolean isComunicavel() {
+        return false; // Por padrão, Entidade não é comunicável, sobrescrever em subclasses se necessário
+    }
 
 
     //se precisar criar metodos cria depois, pois a principio nao vejo a necessidade, no caso no enum de tipoentidade
