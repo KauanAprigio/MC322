933ee34301b953a30fcbcd3ddea54b84100ff88b
DIEGO <d288809@dac.unicamp.br>
2025-05-26 15:11:42 -0300

    adiciona tratamento de exceção para o método aprimorar na interface Aprimoravel

--- LAB04/Code/Aprimoravel.java
+++ LAB04/Code/Aprimoravel.java
@@ -1,5 +1,7 @@
 package LAB04.Code;
 
+import LAB04.Code.Exceptions.ErrorAprimoramentoException;
+
 public interface Aprimoravel {
-    public void aprimorar(int upgrade);
+    public void aprimorar(int upgrade) throws ErrorAprimoramentoException;
 }
