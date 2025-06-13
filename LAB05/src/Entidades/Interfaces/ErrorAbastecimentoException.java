0a9ccd9d7534d830e8ea32bba4ddaead7067f951
DIEGO <d288809@dac.unicamp.br>
2025-06-13 01:16:37 -0300

    tava na pasta errada

--- LAB05/src/Entidades/Interfaces/ErrorAbastecimentoException.java
+++
@@ -1,7 +0,0 @@
-package LAB05.src.Entidades.Interfaces;
-
-public class ErrorAbastecimentoException extends Exception {
-    public ErrorAbastecimentoException(String message) {
-        super(message);
-    }
-}
