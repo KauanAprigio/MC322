3c8ecf37b17eaba9252f08fbc16f7f6b3eb2609d
DIEGO <d288809@dac.unicamp.br>
2025-06-13 01:16:28 -0300

    adiciona a central de comunicacao e implementa o comunicador central como entidade. Ainda nn sei como as msgs vão funcionar ent pode mudar isso td

---
+++ LAB05/src/Entidades/CentralComunicacao.java
@@ -0,0 +1,33 @@
+package LAB05.src.Entidades;
+import java.util.ArrayList;
+
+public class CentralComunicacao {
+    //Atributos
+    private ArrayList<String> mensagens;
+    private int TotalMensagens = 0;
+
+
+    //Construtor
+    public CentralComunicacao(){
+        this.mensagens = new ArrayList<String>();
+    }
+
+
+    //Metodos
+    public void registrarMensagem(String remetente, String msg){ 
+        String entrada = "Mensagem de " + remetente + ": " + msg;
+        mensagens.add(entrada);
+        TotalMensagens++;
+        System.out.println("Mensagem registrada: " + entrada);
+        System.out.println("Total de mensagens: " + TotalMensagens + ".\n");
+    }
+
+    public void exibirMensagens(){ // pode fazer algo se o mensagens estiver vazio retornar um exception...
+        System.out.println("Exibindo todas as mensagens:");
+        for(int i = 0; i < mensagens.size(); i++){
+            System.out.println(mensagens.get(i));
+        }
+    }
+
+    public int getTotalMensagens() { return TotalMensagens; }
+}
--- LAB05/src/Entidades/ComunicadorCentral.java
+++ LAB05/src/Entidades/ComunicadorCentral.java
@@ -1,11 +1,28 @@
 package LAB05.src.Entidades;
 
+
 import LAB05.src.Ambiente.Ambiente;
 import LAB05.src.Entidades.Interfaces.Comunicavel;
+import LAB05.src.Entidades.Interfaces.Entidade;
 import LAB05.src.Entidades.Robos.Robo;
 
-public class ComunicadorCentral implements Comunicavel{
-    private Ambiente ambiente;
+public class ComunicadorCentral extends CentralComunicacao implements Entidade, Comunicavel{
+
+    private final int larguraX = 5;
+    private final int larguraY = 5;
+    private final int larguraZ = 100;
+
+    private final char representacao = 'c';
+    private int pos_x;
+    private int pos_y;
+    private final int pos_z = 0;
+    private Ambiente ambiente; // Ambiente onde o obstáculo está localizado
+
+    public ComunicadorCentral(int pos_x, int pos_y, Ambiente a) {
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.ambiente = a;
+    }
 
     @Override
     public void EnviarMensagem(String msg, Comunicavel remetente) {
@@ -19,5 +36,63 @@ public class ComunicadorCentral implements Comunicavel{
 
     public void LocalizarFogoMaisProx (Robo r) {
 
+    }
+
+    @Override
+    public int getX_1() { 
+        return pos_x;
+    }
+    @Override
+    public int getY_1() {
+        return pos_y;
+    }
+    @Override
+    public int getZ_1() {
+        return pos_z;
+    }
+    @Override
+    public Ambiente getAmbiente() {
+        return ambiente;
+    }
+    @Override
+    public String getId() {
+        return "Comunicador Central";
+    }
+    @Override
+    public String getDescricao() {
+        String descricao = "nn sei ainda";
+        return descricao;
+    }
+    @Override
+    public int getLarguraX() {
+        return larguraX;
+    }
+    @Override
+    public int getLarguraY() {
+        return larguraY;
+    }
+    @Override
+    public int getLarguraZ() {
+        return larguraZ;
+    }
+    @Override
+    public char getRepresentacao() {
+        return representacao;
+    }
+    @Override
+    public TipoEntidade getTipo() {
+        return TipoEntidade.COMUNICADOR;
+    }
+    @Override
+    public int getX_2() {
+        return pos_x + getLarguraX();
+    }
+    @Override
+    public int getY_2() {
+        return pos_y + getLarguraY();
+    }
+    @Override
+    public int getZ_2() {
+        return pos_z + getLarguraZ();
     }
 }
